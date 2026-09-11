7b939af6bdf39d75f3f969a4bd1839d583f15a0a
SkyIsBlood <nwamouloud@gmail.com>
2026-09-11 22:22:40 +0100

    added another solutionto problem 5

--- completed/05_easy_Valid_Parentheses.cpp
+++ completed/05_easy_Valid_Parentheses.cpp
@@ -9,24 +9,46 @@ using namespace std;
 
 class Solution {
 public:
-    bool isValid(string & s) 
+    bool isValid(std::string_view s) 
     {
-        unordered_map<char, char> dict = {{'(',')'},{'{','}'},{'[',']'}};
+
+        size_t len = s.size();
+        if (len % 2 != 0) return false;
+
         std::vector<char> stack;
-        for (char c:s)
+        stack.reserve(len);
+
+        for (const char c : s) {
+            switch (c) {
+                case '(': stack.push_back(')'); break;
+                case '{': stack.push_back('}'); break;
+                case '[': stack.push_back(']'); break;
+                default:
+                    if (stack.empty() || stack.back() != c) return false;
+                    stack.pop_back();
+                    break;
+            }
+        }
+
+
+
+
+        #if 0 //solution 2
+        unordered_map<char, char> dict = {{'(',')'},{'{','}'},{'[',']'}};
+        for (const char  c:s)
         {
             if (!stack.empty()&& c == dict[stack.back()])
             {
-
                 stack.pop_back();
                 continue;
             }
             stack.push_back(c);
         }
+        #endif 0
         return stack.empty();
+        
     }
 };
-
 int main() {
     Solution sol;
     string test = "{}()";
